8bde915813cf18b468cf86bc6eeb35a104ad09f2
Matthew McLeod <matthew.mcleod@mavs.uta.edu>
2018-04-30 14:10:19 -0500

    router and agent initialization

---
+++ communications.py
@@ -0,0 +1,38 @@
+import threading
+import router
+import agent
+
+routing_table = {
+        'A':   {'111': 0, 'B': 4,  'C': 3, 'E': 7},
+        'B':   {'A': 4,   'C': 6,  'L': 5},
+        'C':   {'A': 3,   'B': 6,  'D': 11},
+        'D':   {'C': 11,  'G': 10, 'F': 6, 'L': 9},
+        'E':   {'001': 0, 'A': 7,  'G': 5},
+        'F':   {'100': 0, 'D': 6,  'L': 5},
+        'G':   {'D': 10,  'E': 5},
+        'H':   {'100': 0},
+        'L':   {'B': 5,   'D': 9,  'F': 5},
+        '111': {'A': 0},
+        '001': {'E': 0},
+        '100': {'F': 0,   'H': 0}
+    }
+
+def initialize_routers():
+    routers = []
+    labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'L']
+
+    for r in labels:
+        new_router = threading.Thread(target=router, args=(r, routing_table))
+        routers.append(new_router)
+        new_router.start()
+
+def initialize_agents():
+    agents = []
+    names = {'Ann':  '111',
+             'Chan': '001',
+             'Jan':  '100'}
+
+    for name, id in names.iteritems():
+        new_agent = threading.Thread(target=agent, args=(name, id, routing_table))
+        agents.append(new_agent)
+        new_agent.start()
