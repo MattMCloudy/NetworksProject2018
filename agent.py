ab6475fa0d61718117f5df9f353cf7dfed75404c
Matthew McLeod <matthew.mcleod@mavs.uta.edu>
2018-04-30 14:18:24 -0500

    added logging

--- agent.py
+++ agent.py
@@ -1,5 +1,8 @@
+import logging
+
 class agent():
     def __init__(self, name, id, routing_table):
+        logging.debug('Thread initialized')
         self.name = name
         self.id = id
         self.routing_table = routing_table
--- router.py
+++ router.py
@@ -1,4 +1,7 @@
+import logging
+
 class router():
     def __init__(self, label, routing_table):
+        logging.debug('Thread initialized')
         self.label = label
         self.routing_table = routing_table
