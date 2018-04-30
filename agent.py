1861cc95f0fa279320600f9e9ff0e92aa89ba20b
Matthew McLeod <matthew.mcleod@mavs.uta.edu>
2018-04-30 16:40:05 -0500

    Added thread locking, thread sleep, and implemented listen functionality

--- agent.py
+++ agent.py
@@ -1,5 +1,6 @@
 import threading
 import logging
+import time
 
 logging.basicConfig(level=logging.DEBUG, format='(%(threadName)-10s) %(message)s')
 
@@ -10,10 +11,17 @@ class Agent(threading.Thread):
         self.agent_name = args[0]
         self.id = args[1]
         self.routing_table = args[2]
+        self.message_queue = []
+        self.lock = threading.Lock()
 
     def run(self):
         logging.debug('running')
+        self.listen()
         return
+
+    def process_message(self, message):
+        #TODO
+        return 1
 
     def send_message(self, filename):
         message = ''
@@ -24,6 +32,19 @@ class Agent(threading.Thread):
 
         logging.debug('Message Sent: '+message)
 
+    def listen(self):
+        logging.debug('Listening for incoming messages')
+        while(True):
+            self.lock.acquire()
+            if self.message_queue:
+                for message, i in enumerate(self.message_queue):
+                    logging.debug('Message Received: '+message)
+                    self.process_message(message)
+                    self.message_queue.remove(i)
+            self.lock.release
+            time.sleep(0.5)
+
     def receive_message(self, message):
-        logging.debug('Message Received: '+message)
-        self.send_message('Some sort of acknowledgment')
+        self.lock.acquire()
+        self.message_queue.append(message)
+        self.lock.release()
--- router.py
+++ router.py
@@ -1,5 +1,6 @@
 import threading
 import logging
+import time
 
 logging.basicConfig(level=logging.DEBUG, format='(%(threadName)-10s) %(message)s',)
 
@@ -9,15 +10,34 @@ class Router(threading.Thread):
         logging.debug('Thread initialized')
         self.label = args[0]
         self.routing_table = args[1]
+        self.message_queue = []
+        self.lock = threading.Lock()
 
     def run(self):
         logging.debug('running')
+        self.listen()
         return
 
+    def receive_message(self, message):
+        self.lock.acquire()
+        self.message_queue.append(message)
+        self.lock.release()
+
     def listen(self):
+        logging.debug('Listening for incoming messages')
+        while(True):
+            self.lock.acquire()
+            if self.message_queue:
+                for message, i in enumerate(self.message_queue):
+                    self.process_message(message)
+                    self.message_queue.remove(i)
+            self.lock.release()
+            time.sleep(0.5)
+
+    def process_message(self, message):
         #TODO
         return 1
 
-    def route(self):
+    def route(self, message):
         #TODO
-        return 1
+        return ''
