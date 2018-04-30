b4cc7a846e2ad90e2aed0567302d696572296bb0
Matthew McLeod <matthew.mcleod@mavs.uta.edu>
2018-04-30 13:16:09 -0500

    Scaffolding for routers and agents

---
+++ agent.py
@@ -0,0 +1,5 @@
+class agent():
+    def __init__(self, name, id, routing_table):
+        self.name = name
+        self.id = id
+        self.routing_table = routing_table
---
+++ router.py
@@ -0,0 +1,4 @@
+class router():
+    def __init__(self, label, routing_table):
+        self.label = label
+        self.routing_table = routing_table
